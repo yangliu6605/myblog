1058fcd9d944ab00f3fc7584226d703f683d5a4a
阳哥 <yangliu6105@gmail.com>
2025-11-30 21:54:38 +0800

    Add route guards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import CreatePost from './components/CreatePost';
 import EditPost from './components/EditPost';
 import PostContent from './components/PostContent';
+import { useAuthContext } from './hooks/useAuthContext';
+
+// 路由守卫组件
+const ProtectedRoute = ({ children }) => {
+  const { user } = useAuthContext();
+  return user ? children : <Navigate to="/login" replace />;
+};
 
 const App = () => {
 
@@ -14,10 +21,19 @@ const App = () => {
       <div className='pages'>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/new-post" element={<CreatePost />} />
+          <Route path="/new-post" element={
+            <ProtectedRoute>
+              <CreatePost />
+            </ProtectedRoute>
+          } />
           <Route path="/post/:id" element={<PostContent />} />
-          <Route path="/edit-post/:id" element={<EditPost />} /> 
+          <Route path="/edit-post/:id" element={
+            <ProtectedRoute>
+              <EditPost />
+            </ProtectedRoute>
+          } /> 
           <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
     </Router>
